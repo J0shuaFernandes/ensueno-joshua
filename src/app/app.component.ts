49073bb7e3ae409204e1ae87accc9d5e24fdf755
Joshua Fernandes <33647316+J0shuaFernandes@users.noreply.github.com>
2022-11-02 23:40:02 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ensueno-joshua';
+
+  displayVal = '';
+  word = '';
+  new_word = '';
+
+  getUnique(val:string) {
+  	//val = val.toLowerCase();
+  	this.word = '';
+  	for(let i=0; i<val.length; i++) {
+  		if ((this.word).includes((val[i]).toLowerCase()) == false && (this.word).includes((val[i]).toUpperCase()) == false) {
+  			this.word += val[i];
+  		}
+  	}
+  }
+
 }
